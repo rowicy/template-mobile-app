0aec5ebc1808582d17e25bada2405e4ccdc74fd9
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2025-07-21 01:20:47 +0900

    🐞 fix: remove unused "Explore" tab from TabLayout

--- mobile-app/app/(tabs)/_layout.tsx
+++ mobile-app/app/(tabs)/_layout.tsx
@@ -35,15 +35,6 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
-        options={{
-          title: "Explore",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="paperplane.fill" color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
         name="component-library"
         options={{
           title: "Components",
