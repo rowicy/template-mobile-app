9c379152f2102dac4152fb9e770b0a9bd2916293
Naoki_Ito <117070296+naoki-00-ito@users.noreply.github.com>
2025-07-21 01:04:33 +0900

    🐞 fix: warn routing

--- mobile-app/app/example/index.tsx
+++ mobile-app/app/example/index.tsx
@@ -1,16 +1,5 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import ItemsScreen from "./items-screen";
-import ItemDetailsScreen from "./item-details-screen";
-import { Screens } from "./constants";
-import type { Params } from "./types";
+import Example from "@/components/example";
 
-const Stack = createNativeStackNavigator<Params>();
-
-export default function Example() {
-  return (
-    <Stack.Navigator initialRouteName={Screens.Items}>
-      <Stack.Screen name={Screens.Items} component={ItemsScreen} />
-      <Stack.Screen name={Screens.ItemDetails} component={ItemDetailsScreen} />
-    </Stack.Navigator>
-  );
+export default function ExampleScreen() {
+  return <Example />;
 }
